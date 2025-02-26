1aa1f9ae40bc7d2ab192b27f66f93c9762c6cad9
Paul Martin McNeill <paulmcneill1989@hotmail.co.uk>
2025-02-26 23:28:17 +0000

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+// from your Firebase console > Project settings
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZS_Bfl7Bj4axlFt8Pg3HebYzAbrqBDQs',
+  authDomain: 'commai-b98fe.firebaseapp.com',
+  databaseURL: 'https://commai-b98fe.firebaseio.com',
+  projectId: 'commai-b98fe',
+  storageBucket: 'commai-b98fe.appspot.com',
+  messagingSenderId: '...',
+  appId: '...'
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
