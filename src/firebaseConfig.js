4304b628241f64e7d7b10145af68a090cf729aff
Paul Martin McNeill <paulmcneill1989@hotmail.co.uk>
2025-02-28 22:57:00 +0000

    Update databaseURL in firebaseConfig.js to new Firebase Realtime Database URL

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,11 +3,11 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
-// from your Firebase console > Project settings
 const firebaseConfig = {
   apiKey: 'AIzaSyBZS_Bfl7Bj4axlFt8Pg3HebYzAbrqBDQs',
   authDomain: 'commai-b98fe.firebaseapp.com',
-  databaseURL: 'https://commai-b98fe.firebaseio.com',
+  // Updated databaseURL:
+  databaseURL: 'https://commai-b98fe-default-rtdb.europe-west1.firebasedatabase.app',
   projectId: 'commai-b98fe',
   storageBucket: 'commai-b98fe.appspot.com',
   messagingSenderId: '...',
